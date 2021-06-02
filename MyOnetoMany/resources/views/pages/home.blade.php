862799457ff6926be4a8a3d4e0cbd62b7c846809
Benito <bbxblack@gmail.com>
2021-06-02 10:49:52 +0200

    v0.0

--- MyOnetoMany/resources/views/pages/home.blade.php
+++ MyOnetoMany/resources/views/pages/home.blade.php
@@ -2,6 +2,22 @@
 
 @section('principal')
 
+    {{-- 
+        Employee: 
+
+        - firtsname
+        - lastname
+        - ral
+
+        Task:
+
+        - title
+        -description
+
+        Employee <-1----N-> Task
+        
+        --}}
+
     <h1>HELLO</h1>
     
 @endsection
