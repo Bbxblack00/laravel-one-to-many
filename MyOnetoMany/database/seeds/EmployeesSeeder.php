66cb369b01a7807cee5eeadc3048be2451b8d674
Benito <bbxblack@gmail.com>
2021-06-07 00:12:55 +0200

    v0.6

--- MyOnetoMany/database/seeds/EmployeesSeeder.php
+++ MyOnetoMany/database/seeds/EmployeesSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Employee;
 
-class EmployeeSeeder extends Seeder
+class EmployeesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- MyOnetoMany/database/seeds/TaskSeeder.php
+++ MyOnetoMany/database/seeds/TaskSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Task;
+use App\Employee;
 
 class TaskSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        factory(Task::class, 5) -> create();
+        // factory(Task::class, 5) -> make() -> each(function($task) {
+
+        //     $employee = Employee::inRandomOrder() -> first();
+        //     $task -> employee_id = $employee -> id;
+
+        //     $task -> save();
+        // });
     }
 }
